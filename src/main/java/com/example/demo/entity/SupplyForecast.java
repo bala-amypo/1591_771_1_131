c071ced713f475aa179211ef4a29fe37aef51101
bala-amypo <bala@amypo.in>
2025-12-19 06:31:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/SupplyForecast.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SupplyForecast {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double availableSupplyMW;
+
+    @Column(nullable = false)
+    private Instant forecastStart;
+
+    @Column(nullable = false)
+    private Instant forecastEnd;
+
+    private Instant generatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.generatedAt = Instant.now();
+    }
+}
