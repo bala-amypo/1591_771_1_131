761850e2496b9bc9b68c1ebb20196f1945b44b88
bala-amypo <bala@amypo.in>
2025-12-19 06:30:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/DemandReading.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DemandReading {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Zone zone;
+
+    @Column(nullable = false)
+    private Double demandMW;
+
+    @Column(nullable = false)
+    private Instant recordedAt;
+}
