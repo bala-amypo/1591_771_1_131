bd2a4701d42f84746872f220ca88dfb7b2b6363e
bala-amypo <bala@amypo.in>
2025-12-21 17:47:33 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +13,12 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Smart Grid Load Shedding API")
+                        .version("1.0")
+                        .description("API Documentation for Smart Grid Management"))
                 .servers(List.of(
                         new Server().url("https://9310.408procr.amypo.ai")
                 ));
-        }
+    }
 }
